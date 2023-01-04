d419b5a2d7ac4e57a7e857289f633d8f9c8a9ab1
Red Fox <link.to.my.email@gmail.com>
2023-01-04 16:00:10 +0300

    prep 1-2 DONE

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,62 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // getting user input
+        Console.Write("What is the grade percentage? ");
+        string userInput = Console.ReadLine();
+        float gradePercentage = float.Parse(userInput);
+        // variables to hold grade letter and sign
+        string letter, sign;
+        //  passed sections
+        if (gradePercentage >= 70)
+        {
+            if (gradePercentage >= 90)
+            {
+                letter = "A";
+            }
+            else if (gradePercentage >= 80)
+            {
+                letter = "B";
+            }
+            else
+            {
+                letter = "C";
+            }
+            Console.WriteLine("Congratulations! You successfuly passed this course!");
+        }  
+        //  failed section
+        else
+        {    
+            if (gradePercentage >= 60)
+            {
+                letter = "D";
+            }
+            else
+            {
+                letter = "F";   
+            }    
+            Console.WriteLine("Sorry, looks like you did not succeed in this class.");
+        }
+
+        //  adding sign
+        if (gradePercentage % 10 >= 7)
+        {
+            sign = "+";
+        }
+        else if (gradePercentage % 10 < 3 && gradePercentage % 10 != 0)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }  
+        //  removing sign from A+ and F
+        if (gradePercentage >= 93 || gradePercentage < 60)
+        {
+            sign = "";
+        }
+        // printing the grade and its sign
+        Console.WriteLine($"Your grade is {letter}{sign}"); 
     }
 }
